e43739bc76c4819b1bf835e7bb3b5753c619e52f
Glitch (oceanic-river-lock) <none>
2020-08-05 15:08:01 +0000

    🚐🌮 Checkpoint ./server.js:10873923/73

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const IDler = {
   botID: "740564699594096678",
   botToken: "NzQwNTY0Njk5NTk0MDk2Njc4.Xyq2nw.ilp_a9NlwES79g3ye7RTi2KTk5k",
   botSecret: "kDSx_C7z0FRqNIfeMl9_FbENtRP7BTav",
-  botCallbackURL: "https://fynxcodes.glitch.me/callback",
+  botCallbackURL: "https://fynxcodess.glitch.me/callback",
   sunucuID: "740564967199342642",
   sunucuDavet: "https://discord.gg/ATQWcDx",
   kodLogKanalı: "740564967199342645",
